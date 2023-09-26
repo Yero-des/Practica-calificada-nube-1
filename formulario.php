1490af5eb42fd77f7f0ce9b2004c1a160bfe693e
Yero-des <yero_zc@hotmail.com>
2023-09-26 11:03:23 -0500

    fixed

--- formulario.php
+++ formulario.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 ?>
 <?php include_once "encabezado.php"; ?>
 <div class="row">
@@ -9,12 +7,12 @@
     <h1>Agregar</h1>
     <form action="insertar.php" method="POST">
       <div class="form-group">
-          <label for="nombre">Nombre</label>
-          <input required name="nonbre" type="text" id="nombre" placeholder="Nonbre de mascota” class=" form-control">
+        <label for="nombre">Nombre</label>
+        <input required name="nonbre" type="text" id="nombre" placeholder="Nombre de mascota" class="form-control">
       </div>
       <div class="form-group">
         <label for="edad">Edad</label>
-        <input required name="edad" type="number" id-"edad" placeholder="Edad de mascota" class="form-control">
+        <input required name="edad" type="number" id="edad" placeholder="Edad de mascota" class="form-control">
       </div>
       <button type="submit" class="btn btn-success">Guardar</button>
       <a href="./listar.php" class="btn btn-warning">Ver todas</a>
